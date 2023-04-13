6ad6f568d0a092277543f2fafe3e0750e99adbf9
AnaFSaraiva <saraivanapaula2@gmail.com>
2023-04-13 18:00:18 -0300

    simplifica codigo

--- script_desafio.js
+++ script_desafio.js
@@ -1,87 +1,104 @@
-// VERIFICAÇÃO DO NUMERO DE 1-10
-function verificaNumero(numero) {
-  if (numero > 10) {
-    return 10
+// // VERIFICAÇÃO DO NUMERO DE 1-10
+// function verificaNumero(numero) {
+//   if (numero > 10) {
+//     return 10
+//   }
+//   if (numero < 1) {
+//     return 1
+//   }
+//   return numero
+// }
+
+// // // DESAFIO 1 - ESCADA
+// function montaEscada(numero) {
+
+//   const numeroInserido = verificaNumero(numero)
+
+
+
+//   let escada = "";
+
+//   for (i = 0; i < numeroInserido; i++) {
+
+//     for (j = numeroInserido; j >= 0; j--) {
+//       if (j <= i) {
+//         escada += "#";
+//       } else {
+//         escada += " ";
+//       }
+//     }
+
+//     escada += "\n"
+
+//   }
+
+//   console.log(escada)
+
+// }
+
+// // Escada Diferente
+// function montaEscadaAdicao(numero) {
+
+//   const numeroInserido = verificaNumero(numero)
+
+//   let escada = "";
+
+//   for (i = 0; i < numeroInserido; i++) {
+
+//     for (j = 0; j <= numeroInserido; j++) {
+//       if (j <= numeroInserido - i - 1) {
+//         escada += " ";
+//       } else {
+//         escada += "#";
+//       }
+
+//       escada += "\n"
+
+//     }
+
+//     console.log(escada)
+
+//   }
+// }
+
+// // DESAFIO 3 - PIRAMIDE
+// function montaPiramide(numero) {
+
+//   const numeroInserido = verificaNumero(numero)
+
+//   let escada = "";
+
+//   for (i = 0; i < numeroInserido; i++) {
+
+//     for (j = 0; j < numeroInserido; j++) {
+//       if (j >= numeroInserido - i - 1) {
+//         escada += "#";
+//       }
+//       escada += " ";
+//     }
+
+//     escada += "\n"
+
+//   }
+
+//   console.log(escada)
+
+// }
+
+// DESAFIO 2 - SOMA DOS INTEIROS
+
+const numeroInserido = 5
+const listaSomar = []
+const contar = 1
+
+function numerosAnteriores(numero) {
+
+  for (i = 0; i <= numero - 1; i++) {
+
+    contar++;
+    listaSomar.push(contagem)
+
   }
-  if (numero < 1) {
-    return 1
-  }
-  return numero
+
 }
 
-// // DESAFIO 1 - ESCADA
-function montaEscada(numero) {
-
-  const numeroInserido = verificaNumero(numero)
-
-
-
-  let escada = "";
-
-  for (i = 0; i < numeroInserido; i++) {
-
-    for (j = numeroInserido; j >= 0; j--) {
-      if (j <= i) {
-        escada += "#";
-      } else {
-        escada += " ";
-      }
-    }
-
-    escada += "\n"
-
-  }
-
-  console.log(escada)
-
-}
-
-// Escada Diferente
-function montaEscadaAdicao(numero) {
-
-  const numeroInserido = verificaNumero(numero)
-
-  let escada = "";
-
-  for (i = 0; i < numeroInserido; i++) {
-
-    for (j = 0; j <= numeroInserido; j++) {
-      if (j <= numeroInserido - i - 1) {
-        escada += " ";
-      } else {
-        escada += "#";
-      }
-
-      escada += "\n"
-
-    }
-
-    console.log(escada)
-
-  }
-}
-
-
-// DESAFIO 3 - PIRAMIDE
-function montaPiramide(numero) {
-
-  const numeroInserido = verificaNumero(numero)
-
-  let escada = "";
-
-  for (i = 0; i < numeroInserido; i++) {
-
-    for (j = 0; j < numeroInserido; j++) {
-      if (j >= numeroInserido - i - 1) {
-        escada += "#";
-      }
-      escada += " ";
-    }
-
-    escada += "\n"
-
-  }
-
-  console.log(escada)
-
-}
